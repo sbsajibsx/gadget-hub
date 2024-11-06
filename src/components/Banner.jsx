8542e25ea439b2b3f8b37e9a27204cf8f1c1e423
sbsajibsx <golamrobbanisajib@gmail.com>
2024-11-06 13:00:39 +0600

    added detail page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,8 @@ import bannerImg from "../assets/banner.jpg"
 const Banner = () => {
   const location = useLocation();
   return (
-    <div className="relative">
-      <div className="hero pb-60">
+    <div className="md:relative">
+      <div className="hero md:pb-60">
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
@@ -18,9 +18,9 @@ const Banner = () => {
           </div>
         </div>
       </div>
-      <div className="p-4 border rounded-xl bg-transparent absolute -bottom-2/4">
+      <div className="p-4 border rounded-xl bg-transparent md:absolute md:-bottom-1/2 md:left-1/2 md:transform md:-translate-x-1/2">
             <div>
-              <img className="w-auto h-[450px] rounded-xl" src={bannerImg} alt="" />
+              <img className="w-auto md:w-[700px] lg:w-auto md:h-[400px] lg:h-[500px] rounded-xl" src={bannerImg} alt="" />
             </div>
       </div>
     </div>
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { IoIosHeartEmpty } from "react-icons/io";
 
 const Details = () => {
   const { product_id } = useParams();
@@ -16,47 +18,42 @@ const Details = () => {
     product_image,
   } = gadget;
   return (
-    <div>
-      <div>
-        <div className="bg-[#9538E2]">
-          <div className="mb-8 pt-10 pb-40 w-11/12 md:w-10/12 mx-auto">
-            <h1 className="text-3xl font-bold text-center">Product Details</h1>
-            <p className="text-center">
-              Explore the latest gadgets that will take your experience to the
-              next level. From smart devices to the coolest accessories, we have
-              it all!
-            </p>
+    <div className="md:relative bg-[#9538E2] md:mb-96">
+      <div className="w-9/12 mx-auto md:pt-4 md:pb-72 p-4 space-y-2">
+        <h1 className="font-bold text-4xl text-center">Product Details</h1>
+        <p className="text-center">
+          Explore the latest gadgets that will take your experience to the next
+          level. From smart devices to the coolest accessories, we have it all!
+        </p>
+      </div>
+      <div className="card card-side bg-base-100 shadow-xl md:w-8/12 mx-auto p-4 border rounded-xl md:absolute md:top-[30%] md:left-1/2 md:transform md:-translate-x-1/2 ">
+        <figure>
+          <img className="w-96 h-80" src={product_image} alt={product_title} />
+        </figure>
+        <div className="card-body space-y-2">
+          <h1 className="text-5xl font-bold">{product_title}</h1>
+          <p className="font-bold">Price: {price}</p>
+          <div className="flex">
+            <button className="btn btn-xs bg-[#2f9c0820]">
+              {availability ? "In Stock" : "Out of Stock"}
+            </button>
           </div>
-        </div>
-        <div>
-          <div className="hero min-h-screen">
-            <div className="hero-content md:flex justify-between">
-              <img
-                src={product_image}
-                className="max-w-sm rounded-lg shadow-2xl"
-              />
-              <div className="space-y-2">
-                <h1 className="text-5xl font-bold">{product_title}</h1>
-                <p className="font-bold">Price: {price}</p>
-                <button className="btn btn-xs bg-[#2f9c0820]">
-                  {availability ? "In Stock" : "Out of Stock"}
-                </button>
-                <p>{description}</p>
-                <p className="font-bold text-xl">Specification:</p>
-                <div className="space-y-2">
-                  {Specification.map((p, idx) => (
-                    <ol key={idx}>
-                      <li>{p}</li>
-                    </ol>
-                  ))}
-                </div>
-                <div>
-                    <p className="font-bold text-xl">Rating:</p>
-                    <p>{rating}</p>
-                </div>
-                <button className="btn btn-primary">Get Started</button>
-              </div>
-            </div>
+          <p>{description}</p>
+          <p className="font-bold text-xl">Specification:</p>
+          <div className="space-y-2">
+            {Specification.map((p, idx) => (
+              <ol key={idx}>
+                <li>{p}</li>
+              </ol>
+            ))}
+          </div>
+          <div>
+            <p className="font-bold text-xl">Rating:</p>
+            <p>{rating}</p>
+          </div>
+          <div className="flex items-center">
+          <button className="btn btn-sm bg-[#9538E2] mr-3">Add To Card <MdOutlineShoppingCart /></button>
+          <button className="p-2 rounded-full border"><IoIosHeartEmpty /></button>
           </div>
         </div>
       </div>
